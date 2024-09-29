0378fb5290eaab7f3794fff52bc936d06e767d2a
hifoomin <jakubrataj@protonmail.com>
2024-09-29 07:49:52 +0200

    scorched acres (night) reversion

--- SA.Content/Variants/Stage3/ScorchedAcres.cs
+++ SA.Content/Variants/Stage3/ScorchedAcres.cs
@@ -41,13 +41,44 @@ namespace StageAesthetic.Variants.Stage3
 
         public static void Night(RampFog fog)
         {
-            Skybox.NightSky();
+            fog.fogColorStart.value = new Color32(51, 55, 83, 30);
+            fog.fogColorMid.value = new Color32(30, 29, 66, 130);
+            fog.fogColorEnd.value = new Color32(43, 36, 60, 255);
+            fog.skyboxStrength.value = 0.03f;
             var lightBase = GameObject.Find("Weather, Wispgraveyard").transform;
-            lightBase.GetChild(2).gameObject.SetActive(false);
-            lightBase.Find("Directional Light (SUN)").gameObject.SetActive(false);
-            var sunHolder = lightBase.Find("CameraRelative").Find("SunHolder");
-            sunHolder.gameObject.SetActive(false);
-            AddRain(RainType.Typhoon);
+            lightBase.GetChild(1).GetChild(0).gameObject.SetActive(false); // embers
+            var sunTransform = lightBase.Find("Directional Light (SUN)");
+            Light sunLight = sunTransform.gameObject.GetComponent<Light>();
+            sunLight.color = new Color32(173, 175, 245, 255);
+            sunLight.intensity = 2.3f;
+            sunLight.shadowStrength = 0.35f;
+            sunLight.shadowBias = 0.05f;
+            lightBase.Find("CameraRelative").Find("SunHolder").gameObject.SetActive(false);
+            var eclipse = FindEclipseGameObject(GameObject.Find("Weather, Wispgraveyard").scene);
+            if (eclipse != null)
+            {
+                eclipse.SetActive(true);
+                eclipse.transform.GetChild(1).gameObject.SetActive(false); // lighting
+                eclipse.transform.GetChild(2).gameObject.SetActive(false); // post-processing
+                eclipse.transform.GetChild(4).gameObject.SetActive(false); // weather
+                eclipse.transform.GetChild(3).GetChild(1).gameObject.SetActive(false);
+                eclipse.transform.GetChild(3).GetChild(2).gameObject.SetActive(true);
+
+                var sphere = eclipse.transform.GetChild(3).GetChild(2).gameObject;
+
+                var moonPosition = sphere.transform.position;
+                moonPosition.y = 263;
+
+                sphere.transform.localScale = new Vector3(8, 8, 8);
+
+                var meshRenderer = sphere.GetComponent<MeshRenderer>();
+
+                var coolerMat = new Material(Addressables.LoadAssetAsync<Material>("RoR2/Base/eclipseworld/matEclipseMoon.mat").WaitForCompletion());
+                coolerMat.SetFloat("_AtmosphereStrength", 20f);
+                coolerMat.SetColor("_TintColor", new Color32(10, 10, 17, 255));
+
+                meshRenderer.material = coolerMat;
+            }
         }
 
         public static void Jade(RampFog fog)
