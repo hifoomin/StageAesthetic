874b50273f0c3a7d48c01ce7d7d9d08e630b0fe1
hifoomin <jakubrataj@protonmail.com>
2024-09-29 06:55:12 +0200

    wetland aspect (night) tweaks

--- SA.Content/Variants/Stage2/WetlandAspect.cs
+++ SA.Content/Variants/Stage2/WetlandAspect.cs
@@ -61,6 +61,7 @@ namespace StageAesthetic.Variants.Stage2
 
         public static void Night(RampFog fog)
         {
+            /*
             Skybox.NightSky();
             AddRain(RainType.Rainstorm);
 
@@ -68,6 +69,29 @@ namespace StageAesthetic.Variants.Stage2
             caveOuter.fogColorStart.value = new Color32(14, 111, 160, 0);
             caveOuter.fogColorMid.value = new Color32(66, 76, 43, 89);
             caveOuter.fogColorEnd.value = new Color32(75, 84, 51, 200);
+            */
+
+            Skybox.NightSky(true, true);
+
+            fog.fogColorStart.value = new Color32(64, 76, 70, 15);
+            fog.fogColorMid.value = new Color32(48, 66, 58, 173);
+            fog.fogColorEnd.value = new Color32(47, 60, 48, 255);
+            fog.skyboxStrength.value = 0.001f;
+            fog.fogOne.value = 0.25f;
+            fog.fogZero.value = -0.02f;
+            fog.fogPower.value = 0.9f;
+            fog.fogIntensity.value = 0.937f;
+            fog.fogOne.value = 0.355f;
+            var sunLight = GameObject.Find("Directional Light (SUN)").GetComponent<Light>();
+            sunLight.color = new Color32(109, 182, 185, 255);
+            sunLight.intensity = 0.4f;
+            sunLight.shadowStrength = 0.6f;
+            AddRain(RainType.Rainstorm);
+
+            var caveOuter = GameObject.Find("HOLDER: Hidden Altar Stuff").transform.Find("Blended").gameObject.GetComponent<PostProcessVolume>().profile.GetSetting<RampFog>();
+            caveOuter.fogColorStart.value = new Color32(14, 111, 160, 0);
+            caveOuter.fogColorMid.value = new Color32(66, 76, 43, 89);
+            caveOuter.fogColorEnd.value = new Color32(75, 84, 51, 255);
         }
 
         public static void Void(RampFog fog)
